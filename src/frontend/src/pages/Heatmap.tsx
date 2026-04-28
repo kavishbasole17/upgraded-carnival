154cc5eb4edd0ceb76aab6e61b917cd808a27fd6
kavishbasole17 <kavishbasole28@gmail.com>
2026-04-28 14:38:52 +0530

    KAVDUMBS FIX: vercel deployment fixed

--- src/frontend/src/pages/Heatmap.tsx
+++ src/frontend/src/pages/Heatmap.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { MapPin } from "lucide-react";
 import api from "../services/api";
 import MapLayer from "../components/MapLayer";
